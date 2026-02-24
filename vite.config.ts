41d9b635d0067c19c4a8712c0e4ef225d49b4f79
tenyega <tenyega23@gmail.com>
2026-02-24 23:01:27 +0100

    changes in

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [
-      react(), 
-      tailwindcss(),
-      legacy({
-        targets: ['ios >= 13', 'safari >= 13'],
-      })
-    ],
+    plugins: [react(), tailwindcss()],
     build: {
       target: ['es2015', 'safari13'],
     },
